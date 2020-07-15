4706add2314a76a8f07cdcf94acf3e183185e888
Yoshi NRT <32790691+yoshinrt@users.noreply.github.com>
2020-07-15 14:40:24 +0900

    初期ファイル登録

---
+++ prevent_ss.cpp
@@ -0,0 +1,117 @@
+﻿/*****************************************************************************
+
+		windows application skelton  v1.00
+		Copyright (C) 2000 by Deyu Deyu Software
+
+*****************************************************************************/
+
+#include <windows.h>
+#include <tchar.h>
+
+/*** constant definition ****************************************************/
+
+#define WINDOW_TITLE	_T( "Sample window" )		// NULL 可
+#define WINDOW_CLASS	_T( "SampleAppClass" )
+
+/*** gloval var definition **************************************************/
+
+const TCHAR	*szWinClassName = WINDOW_CLASS;
+
+HINSTANCE	g_hInst;
+HWND		g_hWnd;
+
+/*** window procedure *******************************************************/
+
+LRESULT CALLBACK WindowProc(
+	HWND	hWnd,
+	UINT	Message,
+	WPARAM	wParam,
+	LPARAM	lParam ){
+	
+	POINT	Point;
+	
+	switch( Message ){
+	  case WM_TIMER:
+		GetCursorPos( &Point );
+		SetCursorPos( Point.x ^ 1, Point.y );
+		break;
+		
+	  case WM_DESTROY:				/* terminated by user					*/
+		PostQuitMessage( 1 );
+		break;
+		
+	  default:
+		return( DefWindowProc( hWnd, Message, wParam, lParam ));
+	}
+	return( 0 );
+}
+
+/*** main procedure *********************************************************/
+
+int WINAPI _tWinMain(
+	HINSTANCE	hInst,
+	HINSTANCE	hPrevInst,
+	LPTSTR		lpCmdLine,
+	int			iCmdShow ){
+	
+	/* for creating window */
+	
+	HWND		hWnd;
+	MSG			Msg;
+	WNDCLASS	wcl;
+	
+	/*** cleate a window ****************************************************/
+	
+	g_hInst = hInst;
+	
+	/* define a window class */
+	
+	wcl.hInstance		= hInst;
+	wcl.lpszClassName	= szWinClassName;
+	wcl.lpfnWndProc		= WindowProc;
+	wcl.style			= 0;
+	
+	wcl.hIcon			= LoadIcon( NULL, IDI_APPLICATION );
+	wcl.hCursor			= LoadCursor( NULL, IDC_ARROW );
+	wcl.lpszMenuName	= NULL;
+	
+	wcl.cbClsExtra		= 0;
+	wcl.cbWndExtra		= 0;
+	
+	wcl.hbrBackground	=( HBRUSH )GetStockObject( WHITE_BRUSH );
+	
+	/* register the window class */
+	
+	if( !RegisterClass( &wcl )) return( 1 );
+	
+	g_hWnd = hWnd = CreateWindow(
+		szWinClassName,
+		WINDOW_TITLE,			// window title
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,			// x
+		CW_USEDEFAULT,			// y
+		CW_USEDEFAULT,			// w
+		CW_USEDEFAULT,			// h
+		HWND_DESKTOP,
+		NULL,
+		hInst,
+		NULL
+	);
+	
+	/* display the window */
+	
+	//ShowWindow( hWnd, iCmdShow );
+	UpdateWindow( hWnd );
+	
+	// タイマ設定
+	SetTimer( g_hWnd, 0/*ID_TIMER*/, 290 * 1000, NULL );
+	
+	/* create the message loop */
+	
+	while( GetMessage( &Msg, NULL, 0, 0 )){
+		TranslateMessage( &Msg );
+		DispatchMessage( &Msg );
+	}
+	
+	return( 0 );
+}
